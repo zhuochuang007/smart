2aa8264cd5c87d3b8f79421c63bc1723488d82f6
chuzhisheng <399608459@qq.com>
2016-05-28 04:24:36 +0800

    修改配置js

--- smart-resources/src/main/resources/web/scripts/config/config.js
+++ smart-resources/src/main/resources/web/scripts/config/config.js
@@ -1,12 +1,11 @@
 require.config({
-	 baseUrl: "../scripts",
 	 paths: {
-		     jquery:"vendor/jquery-1.12.4.min",
-		     bootstrap:"vendor/bootstrap.min",
-		     angular:"vendor/angular.min"
+		     jquery:"../vendor/jquery-1.12.4.min",
+		     bootstrap:"../vendor/bootstrap.min",
+		     angular:"../vendor/angular.min"
 		    },
 	 shim: {
 		    'angular': {exports: 'angular'}
 		   }
 	});
-require(['jquery', 'bootstrap','angular'],function($, canvas, sub) {});
+require(['jquery', 'bootstrap','angular'],function($, bootstrap, angular) {});
